1ec3364563081b811cab333550c8ff59e4f837b0
Rasmita9428 <rasmitaprajapati95@gmail.com>
2021-02-06 15:34:31 +0530

    Image Detail Screen

---
+++ app/src/main/java/com/example/nasagalleryapp/SingleImageScreen.java
@@ -0,0 +1,14 @@
+package com.example.nasagalleryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SingleImageScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_image_screen);
+    }
+}
