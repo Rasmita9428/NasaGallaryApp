bb04a4a475e2b4962498fb90b0d2da812d3083e1
Rasmita9428 <rasmitaprajapati95@gmail.com>
2021-02-06 15:28:14 +0530

    Images Grid Screen

---
+++ app/src/main/java/com/example/nasagalleryapp/User.java
@@ -0,0 +1,91 @@
+
+package com.example.nasagalleryapp;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class User {
+
+    private String copyright;
+    private String date;
+    private String explanation;
+    private String hdurl;
+    private String mediaType;
+    private String serviceVersion;
+    private String title;
+    private String url;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    public String getCopyright() {
+        return copyright;
+    }
+
+    public void setCopyright(String copyright) {
+        this.copyright = copyright;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getExplanation() {
+        return explanation;
+    }
+
+    public void setExplanation(String explanation) {
+        this.explanation = explanation;
+    }
+
+    public String getHdurl() {
+        return hdurl;
+    }
+
+    public void setHdurl(String hdurl) {
+        this.hdurl = hdurl;
+    }
+
+    public String getMediaType() {
+        return mediaType;
+    }
+
+    public void setMediaType(String mediaType) {
+        this.mediaType = mediaType;
+    }
+
+    public String getServiceVersion() {
+        return serviceVersion;
+    }
+
+    public void setServiceVersion(String serviceVersion) {
+        this.serviceVersion = serviceVersion;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
