1286d5de580519239c350a10007e8ab9ddf622c8
Rasmita9428 <rasmitaprajapati95@gmail.com>
2021-02-09 13:37:46 +0530

    Pending Code

--- app/src/main/java/com/example/nasagalleryapp/MainActivity.java
+++ app/src/main/java/com/example/nasagalleryapp/MainActivity.java
@@ -2,13 +2,99 @@ package com.example.nasagalleryapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    GridView gridView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(InternetConnection.checkConnection(MainActivity.this)==true) {
+            gridView = findViewById(R.id.gridviewgallery);
+            String jsonFileString = Utils.getJsonFromAssets(getApplicationContext(), "data.json");
+
+            Gson gson = new Gson();
+            Type listUserType = new TypeToken<List<User>>() {
+            }.getType();
+
+            List<User> users = gson.fromJson(jsonFileString, listUserType);
+
+            gridView.setAdapter(new ImageAdapter(this, users));
+            gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                public void onItemClick(AdapterView<?> parent,
+                                        View v, int position, long id) {
+                    // Send intent to SingleViewActivity
+                    Intent i = new Intent(getApplicationContext(), SingleImageScreen.class);
+                    // Pass image index
+                    i.putExtra("id", position);
+                    startActivity(i);
+                }
+            });
+        }else{
+            Toast.makeText(MainActivity.this, "No Internet Connection!", Toast.LENGTH_SHORT).show();
+        }
     }
+
+    public static class ImageAdapter extends BaseAdapter {
+        public Context context;
+        List<User> users;
+        LayoutInflater inflter;
+
+        public ImageAdapter(Context context, List<User> users) {
+            this.users=users;
+            this.context=context;
+            inflter = (LayoutInflater.from(context));
+
+        }
+
+        @Override
+        public int getCount() {
+            return users.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return users.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View view, ViewGroup parent) {
+            view = inflter.inflate(R.layout.customlist, null); // inflate the layout
+            ImageView icon = (ImageView) view.findViewById(R.id.icon); // get the reference of ImageView
+            TextView text = (TextView) view.findViewById(R.id.text); // get the reference of ImageView
+            text.setText(users.get(position).getTitle());
+            Glide.with(context)
+                    .load(users.get(position).getUrl())
+                    .placeholder(R.drawable.ic_launcher_background)
+                    .centerCrop()
+                    .into(icon);
+            return view;
+
+        }
+    }
+
 }
--- app/src/main/java/com/example/nasagalleryapp/SingleImageScreen.java
+++ app/src/main/java/com/example/nasagalleryapp/SingleImageScreen.java
@@ -2,13 +2,44 @@ package com.example.nasagalleryapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.List;
 
 public class SingleImageScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_single_image_screen);
+        setContentView(R.layout.activity_single_image_screen); // Get intent data
+        Intent i = getIntent();
+
+        // Selected image id
+        int position = i.getExtras().getInt("id");
+        ImageView imageView = (ImageView) findViewById(R.id.SingleView);
+        TextView tv_title = (TextView) findViewById(R.id.tv_title);
+        TextView tv_desc = (TextView) findViewById(R.id.tv_desc);
+        String jsonFileString = Utils.getJsonFromAssets(getApplicationContext(), "data.json");
+
+        Gson gson = new Gson();
+        Type listUserType = new TypeToken<List<User>>() { }.getType();
+
+        List<User> users = gson.fromJson(jsonFileString, listUserType);
+        tv_title.setText(users.get(position).getTitle());
+        tv_desc.setText(users.get(position).getExplanation());
+
+        Glide.with(this)
+                .load(users.get(position).getUrl())
+                .placeholder(R.drawable.ic_launcher_background)
+                .centerCrop()
+                .into(imageView);
     }
 }
